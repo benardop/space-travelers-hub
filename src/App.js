b836097e6d14c9493a89c68203283cf3c361fa40
benardop <benard.oc.p@gmail.com>
2022-10-06 21:23:11 +0300

    Add App.js script file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import Rockets from './components/Rockets';
-import Missions from './components/Missions';
-import MyProfile from './components/MyProfile';
+import Header from './components/Navbar';
+import Missions from './components/missions/Missions';
+import Myprofile from './components/myprofile/Myprofile';
+import Rockets from './components/rockets/Rockets';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/myprofile" element={<MyProfile />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="myprofile" element={<Myprofile />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
